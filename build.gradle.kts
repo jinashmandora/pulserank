9d17220f69924d52e75572e0c14deb53f046f9f5
jinashmandora <jinash.mandora@gmail.com>
2026-07-10 17:03:33 +0530

    Configure Gradle multi-module build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,23 @@
 plugins {
-    id("java")
+    base
 }
 
-group = "com.pulserank"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+allprojects {
+    group = "com.pulserank"
+    version = "1.0-SNAPSHOT"
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
 
-tasks.test {
-    useJUnitPlatform()
-}
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+//}
+//
+//tasks.test {
+//    useJUnitPlatform()
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
 rootProject.name = "pulserank"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+include(
+    "common",
+    "event-model",
+    "product-service",
+    "event-service",
+    "query-service",
+    "event-generator",
+    "flink-job"
+)
